a00aad7966e0a4fbee4ecfa1fae4b8ab1759fb75
Abhishek-Iyer1 <iyer.abhishek18@gmail.com>
2026-04-06 18:27:27 -0400

    feat(extract2DFeatures): Implemented basic extraction of 2D features [uniform point selection and edge case rejection pending]

--- src/anuloam/src/featureExtraction.cpp
+++ src/anuloam/src/featureExtraction.cpp
@@ -41,15 +41,70 @@ public:
         }
     };
 
+    /**
+     * @brief extract edge and patch features from 2D scans (rings)
+     * @param ring 2D Pointcloud with all the points from the same ring arranged in a consecutive order
+     * @param neighbours number of neighbours to consider in the set when calculating roughness (|S|, cardinality of set)
+     * @param edgeThresh the roughness threshold above which a point is considered an edge
+     * @param planarThresh the roughness threshold below which a point is considered a patch
+     * @param totalFeatures the number of total features to extract from a 2D scan
+     */
+    void extract2DFeatures(const pcl::PointCloud<PointXYZIR>& ring, int neighbours, float edgeThresh, float planarThresh, int totalFeatures) {
+        
+        // TODO: could be multi-threaded since each thread only has read access and needs to store the roughness
+        size_t scanSize = ring.size();
+        size_t currentFeatures = 0;
+
+        for (size_t ind=0; ind<scanSize; ind++) {
+            // calculate roughness of point
+            int setStart = -(neighbours / 2);
+            int setEnd =  (neighbours / 2);
+
+            float sumX = 0, sumY = 0, sumZ = 0;
+
+            for (int j = setStart; j <= setEnd; j++) {
+                if (j == 0) continue;
+                size_t wrapped = (ind + j + scanSize) % scanSize;
+                sumX += ring[ind].x - ring[wrapped].x;
+                sumY += ring[ind].y - ring[wrapped].y;
+                sumZ += ring[ind].z - ring[wrapped].z;
+                // std::printf("Differences are: %f, %f, %f\n", sumX, sumY, sumZ);
+            }
+            float norm = (ring[ind].x*ring[ind].x + ring[ind].y*ring[ind].y + ring[ind].z*ring[ind].z);
+            float roughness = (sumX * sumX + sumY * sumY + sumZ * sumZ) / (neighbours * neighbours * norm);
+            
+            // std::printf("Norm is: %f\tRoughness is: %f\n", norm, roughness);
+
+            // if above a threshold (i.e. edge), then add to edge container
+            if ( roughness > edgeThresh) {
+                _edges.push_back(ring[ind]);
+                currentFeatures++;
+            }
+
+            // if below a certain threshold (i.e. patch), then add to the patch container
+            else if ((roughness < planarThresh) && (currentFeatures < totalFeatures)) {
+                _patches.push_back(ring[ind]);
+                currentFeatures++;
+            }
+        }
+
+        // TODO: Non Maximal Suppression equivalent -> get an even spread of features
+
+        // TODO: Rejecting edge cases mentioned in the LOAM Paper (https://frc.ri.cmu.edu/~zhangji/publications/RSS_2014.pdf)
+    }
+
     // TODO: How to protect against getRings() called when rings is empty?
+    // TODO: Don't need to store R in the point if I already have them in rings
     std::vector<pcl::PointCloud<PointXYZIR>> getRings() { return _rings; }
-    
     pcl::PointCloud<PointXYZIR> getPCL() {return _pcl;}
-    
+    pcl::PointCloud<PointXYZIR> getFeatures() {return _edges + _patches;}
 
 private:
     pcl::PointCloud<PointXYZIR> _pcl;
     std::vector<pcl::PointCloud<PointXYZIR>> _rings;
+    pcl::PointCloud<PointXYZIR> _edges;
+    pcl::PointCloud<PointXYZIR> _patches;
+    pcl::PointCloud<PointXYZIR> _features;
 };
 
 class FeatureExtraction : public rclcpp::Node {
@@ -75,14 +130,26 @@ private:
     std::vector<pcl::PointCloud<PointXYZIR>> rings = LF.getRings();
 
     // Convert PCL -> ROS message and publish
+    pcl::Indices indices(200);
+    std::iota(indices.begin(), indices.end(), 0);
+
+    LF.extract2DFeatures(rings[0], 10, 0.25, 1e-4, 25);
+
+    pcl::PointCloud<PointXYZIR> features = LF.getFeatures();
+
+    // for (size_t i = 0; i < subRing->points.size(); i++) {
+    //     float azimuth = std::atan2(subRing->points[i].y, subRing->points[i].x) * 180.0 / M_PI;
+    //     RCLCPP_INFO(this->get_logger(), "Point %zu: azimuth = %.2f°", i, azimuth);
+    // }
+
     sensor_msgs::msg::PointCloud2 output;
-    pcl::toROSMsg(rings[0], output);
+    pcl::toROSMsg(features, output);
     output.header = msg->header;
     pub_->publish(output);
 
     // Convert PCL -> ROS message and publish
     sensor_msgs::msg::PointCloud2 outputTest;
-    pcl::toROSMsg(rings[1], outputTest);
+    pcl::toROSMsg(rings[0], outputTest);
     outputTest.header = msg->header;
     pubTest_->publish(outputTest);
   }
