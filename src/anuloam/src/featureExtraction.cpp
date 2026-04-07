7100e9dea59565d86298bfcf9e31b1dba178bb9c
Abhishek-Iyer1 <iyer.abhishek18@gmail.com>
2026-04-06 21:40:43 -0400

    feat(extract2DFeatures): calculating k roughest and smoothest points per section, added distance thresholding

--- src/anuloam/src/featureExtraction.cpp
+++ src/anuloam/src/featureExtraction.cpp
@@ -20,6 +20,12 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(PointXYZIR,
     (std::uint16_t, ring, ring)
 )
 
+struct PointRoughness {
+    size_t index;
+    float roughness;
+};
+
+
 class LidarFrame {
 public:
     LidarFrame(const pcl::PointCloud<PointXYZIR>& cloud) : _pcl (cloud) {};
@@ -41,6 +47,30 @@ public:
         }
     };
 
+    float computeRoughness(const pcl::PointCloud<PointXYZIR>& ring, size_t ind) {
+        
+        float norm = ring[ind].x * ring[ind].x + ring[ind].y * ring[ind].y + ring[ind].z * ring[ind].z;
+        if (norm < minRange * minRange || norm > maxRange * maxRange) {
+            return -1.0f; // r < 0 filtered out later
+        }
+
+        size_t scanSize = ring.size();
+        int setStart = -(neighbours / 2);
+        int setEnd = (neighbours / 2);
+
+        float sumX = 0, sumY = 0, sumZ = 0;
+
+        for (int j = setStart; j <= setEnd; j++) {
+            if (j == 0) continue;
+            size_t wrapped = (ind + j + scanSize) % scanSize;
+            sumX += ring[ind].x - ring[wrapped].x;
+            sumY += ring[ind].y - ring[wrapped].y;
+            sumZ += ring[ind].z - ring[wrapped].z;
+        }
+
+        return (sumX * sumX + sumY * sumY + sumZ * sumZ) / (neighbours * neighbours * norm);
+    }
+
     /**
      * @brief extract edge and patch features from 2D scans (rings)
      * @param ring 2D Pointcloud with all the points from the same ring arranged in a consecutive order
@@ -56,49 +86,69 @@ public:
         size_t scanSize = ring.size();
         int numSections = 8;
 
-        int setStart = -(neighbours / 2);
-        int setEnd =  (neighbours / 2);
-
         for (size_t section=1; section <= numSections; section++) {
             
             size_t startInd = static_cast<size_t>(static_cast<float>(section - 1) / numSections * scanSize);
             size_t endInd = static_cast<size_t>(static_cast<float>(section) / numSections * scanSize);
             size_t currentFeatures = 0;
             size_t sectionFeatures = static_cast<size_t>(totalFeatures / numSections);
+            int edgeFeaturesPerSection = 2;
+            int planarFeaturesPerSection = 4;
 
             // std::printf("Current Section: %lu, startInd: %lu, endInd: %lu, sectionFeatures: %lu", section/numSections, startInd, endInd, sectionFeatures);
 
+            std::vector<PointRoughness> roughnesses;
+            roughnesses.reserve(endInd - startInd);
+
             for (size_t ind = startInd; ind < endInd; ind++) {
-                // calculate roughness of point
-                float sumX = 0, sumY = 0, sumZ = 0;
+                float r = computeRoughness(ring, ind);
+                if (r < 0) continue;
+                roughnesses.push_back({ind, r});
+            }
 
-                for (int j = setStart; j <= setEnd; j++) {
-                    if (j == 0) continue;
-                    size_t wrapped = (ind + j + scanSize) % scanSize;
-                    sumX += ring[ind].x - ring[wrapped].x;
-                    sumY += ring[ind].y - ring[wrapped].y;
-                    sumZ += ring[ind].z - ring[wrapped].z;
-                    // std::printf("Differences are: %f, %f, %f\n", sumX, sumY, sumZ);
-                }
+            // append k roughest points (i.e. edges) to _edges
+            size_t kEdge = std::min(static_cast<size_t>(edgeFeaturesPerSection), roughnesses.size());
 
-                // Note: Mathematically speaking we are calculating roughness^2 to save computations on square root operations
-                float norm = (ring[ind].x*ring[ind].x + ring[ind].y*ring[ind].y + ring[ind].z*ring[ind].z);
-                float roughness = (sumX * sumX + sumY * sumY + sumZ * sumZ) / (neighbours * neighbours * norm);
+            std::partial_sort(
+                roughnesses.begin(), 
+                roughnesses.begin() + kEdge, 
+                roughnesses.end(),
+                [](const auto& a, const auto& b) { return a.roughness > b.roughness; }
+            );
 
-                // std::printf("Norm is: %f\tRoughness is: %f\n", norm, roughness);
-
-                // if above a threshold (i.e. edge), then add to edge container
-                if (( roughness > edgeThresh) && (currentFeatures < sectionFeatures)) {
-                    _edges.push_back(ring[ind]);
-                    currentFeatures++;
-                }
-
-                // if below a certain threshold (i.e. patch), then add to the patch container
-                else if ((roughness < planarThresh) && (currentFeatures < sectionFeatures)) {
-                    _patches.push_back(ring[ind]);
-                    currentFeatures++;
+            for (size_t i = 0; i < edgeFeaturesPerSection && i < roughnesses.size(); i++) {
+                if (roughnesses[i].roughness > edgeThresh) {
+                    _edges.push_back(ring[roughnesses[i].index]);
                 }
             }
+            
+            // append k smoothest points (i.e. patches) to _patches
+            size_t kPatch = std::min(static_cast<size_t>(planarFeaturesPerSection), roughnesses.size());
+
+            std::partial_sort(
+                roughnesses.begin(), 
+                roughnesses.begin() + kPatch, 
+                roughnesses.end(),
+                [](const auto& a, const auto& b) { return a.roughness < b.roughness; }
+            );
+
+            for (size_t i = 0; i < planarFeaturesPerSection && i < roughnesses.size(); i++) {
+                if (roughnesses[i].roughness < planarThresh) {
+                    _patches.push_back(ring[roughnesses[i].index]);
+                }
+            }
+
+            // // if above a threshold (i.e. edge), then add to edge container
+            // if (( roughness > edgeThresh) && (currentFeatures < sectionFeatures)) {
+            //     _edges.push_back(ring[ind]);
+            //     currentFeatures++;
+            // }
+
+            // // if below a certain threshold (i.e. patch), then add to the patch container
+            // else if ((roughness < planarThresh) && (currentFeatures < sectionFeatures)) {
+            //     _patches.push_back(ring[ind]);
+            //     currentFeatures++;
+            // }
         }
 
         // TODO: Rejecting edge cases mentioned in the LOAM Paper (https://frc.ri.cmu.edu/~zhangji/publications/RSS_2014.pdf)
@@ -133,6 +183,8 @@ private:
     float edgeThresh = 0.25; 
     float planarThresh = 1e-4; //1e-4; 
     int totalFeatures = 32;
+    float minRange = 1.0;
+    float maxRange = 20.0;
 };
 
 class FeatureExtraction : public rclcpp::Node {
